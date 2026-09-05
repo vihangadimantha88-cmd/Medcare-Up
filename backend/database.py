11677c774e1027381467f204687f87072204d836
nuha67 <nuhasabry020306@gmail.com>
2026-09-06 03:02:18 +0530

    Update database connection URL

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost:5432/medcare"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:medcare67@database1.cutqoo88kac9.us-east-1.rds.amazonaws.com:5432/medcare_db"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
